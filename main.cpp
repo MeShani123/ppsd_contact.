9688112f08ffbb29dc2cc2357bc37d59492b41c9
Sajid-63 <sajidhk63@gmail.com>
2019-12-13 20:29:05 +0500

    Version 2( constructors)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 ////// Contact Management System
 
- #include <iostream>
- #include <string>
- #include <fstream>
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
+class Contact
+{
+    string name;
+    string number;
+    string Email;
+    string city;
+public:
+    Contact()
+    {
+        name="\0";
+        number="\0";
+        Email="\0";
+        city="\0";
+    }///Non Parameterized Constructor End
+    Contact(string name, string Email, string number, string city)
+    {
+        this->name=name;
+        this->Email=Email;
+        this->number=number;
+        this->city=city;
+    }///Parameterized Constructor End
+
+
+};
+int main()
+{
+
+    return 0;
+}
